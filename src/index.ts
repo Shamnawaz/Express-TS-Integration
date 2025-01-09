115ed99dee05947f21e3754f729b4d8e45bec98c
Shamnawaz <shamnawaz.subakhan.sio@gmail.com>
2025-01-09 14:31:33 +0100

    Express setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hi there");
+import express, { Request, Response } from 'express'
+
+const app = express();
+const port = 3080;
+
+app.get('/', (req: Request, res: Response): void => {
+    res.send(`
+        <div>   
+            <h1>Hi there!</h1>
+        </div>
+        `)
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${ port }`);
+})
