a836615f6c100f8a8278dca04d97de0067b2dd34
Shamnawaz <shamnawaz.subakhan.sio@gmail.com>
2025-01-09 16:28:42 +0100

    Login/Logout Page + Protected Routes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from 'express'
 import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
 
 const app = express();
 const port = 3080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['fredfs'] }));
 app.use(router);
 
 app.listen(port, () => {
