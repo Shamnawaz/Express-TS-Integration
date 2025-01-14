a5bef629ccc16624c5f6b512a732e375af1f78f8
Shamnawaz <shamnawaz.subakhan.sio@gmail.com>
2025-01-14 18:40:46 +0100

    Fixing Routes

--- src/controllers/LoginControllers.ts
+++ src/controllers/LoginControllers.ts
@@ -31,7 +31,12 @@ class LoginController {
         } else {
             res.send('Invalid email or password');
         }
-        
+    }
+
+    @get('/logout')
+    getLogout(req: Request, res: Response) {
+        req.session = undefined;
+        res.redirect('/');
     }
 }
 
