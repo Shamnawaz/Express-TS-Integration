c902efea4ebe443b20e33f3a20234a9750423f31
Shamnawaz <shamnawaz.subakhan.sio@gmail.com>
2025-01-14 16:16:01 +0100

    Controller decorators + loginController

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express'
 import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
+import { router as controllerRouter } from './controllers/decorators/controller'
+import  './controllers/LoginControllers';
 
 const app = express();
 const port = 3080;
@@ -9,6 +11,7 @@ const port = 3080;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['fredfs'] }));
 app.use(router);
+app.use(controllerRouter);
 
 app.listen(port, () => {
     console.log(`Listening on port ${ port }`);
