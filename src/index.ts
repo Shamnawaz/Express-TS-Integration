e2964e5378fbf62603c47f70ee7aed69f6972294
Shamnawaz <shamnawaz.subakhan.sio@gmail.com>
2025-01-14 16:27:12 +0100

    Fixups + Singleton Pattern

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express'
 import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
-import { router as controllerRouter } from './controllers/decorators/controller'
+import { AppRouter } from './AppRouter';
 import  './controllers/LoginControllers';
 
 const app = express();
@@ -11,7 +11,7 @@ const port = 3080;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['fredfs'] }));
 app.use(router);
-app.use(controllerRouter);
+app.use(AppRouter.getInstance());
 
 app.listen(port, () => {
     console.log(`Listening on port ${ port }`);
